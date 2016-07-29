9eb182f418b216b4f0c209cc163e017abcbfd215
Abdul Muhaymin Arif <mampary@hotmail.com>
2016-07-29 11:51:42 +0530

    Updated File Structure

--- Samples/Xamarin.Droid Digits Sample/MainActivity.cs
+++ Samples/Xamarin.Droid Digits Sample/MainActivity.cs
@@ -12,7 +12,7 @@ using Bindings.DigitsKit;
 namespace Fabric_Demo
 {
     [Activity(Label = "Fabric_Demo", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity, IAuthCallback
+    public class MainActivity : Activity ,IAuthCallback
     {
         // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
         private static string TWITTER_KEY = "M3Z6o5x4yczZkBfEuJDdakg0t";
@@ -28,24 +28,27 @@ namespace Fabric_Demo
             Toast.MakeText(this, "Number " + p0.PhoneNumber + " verified", ToastLength.Long).Show();
         }
 
-        protected override void OnCreate(Bundle bundle)
+        async protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
-            TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+           TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
             Fabric.With(this, new TwitterCore(authConfig), new Digits());
          
             Button button = FindViewById<Button>(Resource.Id.MyButton);
+            var result await Myfunction();
 
-            //custom Action method
+//            custom Action method
             button.Click += delegate { Digits.Authenticate(this); };
 
-            //with Digits Authenticate Button
+   //         with Digits Authenticate Button
 
             DigitsAuthButton digitsButton = (DigitsAuthButton)FindViewById(Resource.Id.auth_button);
             digitsButton.Text = "Hey Authenticate Me";
             digitsButton.SetCallback(this);
         }
+
+
     }
 }
 
---
+++ Samples/Xamarin.iOS Digits Sample/ViewController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using FabricSdk;
+using DigitsKit;
+using UIKit;
+
+namespace Xamarin.iOS_Digits_Sample
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+       
+
+        private void something(IDigitsSession arg1, ErrorCode arg2)
+        {
+            var res = arg1.PhoneNumber;
+        }
+
+        partial void UIButton12_TouchUpInside(UIButton sender)
+        {
+            var digits = Digits.Instance;
+            digits.Authenticate(something);
+        }
+    }
+}
